3ed27a13cf5436119e74c76dab32e178a2c3529a
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2021-07-21 20:47:23 +0300

    Добавьте файлы проекта.

---
+++ Homework_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5
+{
+  class Program
+  {
+    static void Main()
+    {
+      //Нарисовать треугольник
+      Console.Write("Введите символ, с помощью которого будет начертан треугольник: ");
+      char line = char.Parse(Console.ReadLine());
+      Console.Write("Введите высоту треугольника: ");
+      int height = int.Parse(Console.ReadLine());
+      for (int i = 0; i < height; i++)
+      {
+        for (int j = 0; j < i+1; j++)
+        {
+            Console.Write(line);
+        }
+        Console.WriteLine();
+      }
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Triangle_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_2
+{
+  class Program
+  {
+    static void Main()
+    {
+
+      //Нарисовать треугольник
+      Console.Write("Введите символ, с помощью которого будет начертан треугольник: ");
+      char line = char.Parse(Console.ReadLine());
+      Console.Write("Введите высоту треугольника: ");
+      int height = int.Parse(Console.ReadLine());
+      for (int i = height; i > 0; i--)
+      {
+        for (int j = 0; j < i; j++)
+        {
+          Console.Write(line);
+        }
+        Console.WriteLine();
+      }
+      Console.ReadLine();
+    }
+  }
+}
